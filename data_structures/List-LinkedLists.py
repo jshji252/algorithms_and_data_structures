bd38e9054f9d6954c8daa1007d4b30f50a806b50
jshji252 <jasonseunghoonji@gmail.com>
2021-06-29 16:08:28 +1000

    Added Runner Technique
    Runner technique added

---
+++ data_structures/List-LinkedLists.py
@@ -0,0 +1,164 @@
+# The code below was written by me while completing a course from Codecademy.
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
+
+# Swap Function
+def swap_nodes(input_list, val1, val2):
+  print(f'Swapping {val1} with {val2}')
+
+  node1_prev = None
+  node2_prev = None
+  node1 = input_list.head_node
+  node2 = input_list.head_node
+
+  if val1 == val2:
+    print("Elements are the same - no swap needed")
+    return
+
+  while node1 is not None:
+    if node1.get_value() == val1:
+      break
+    node1_prev = node1
+    node1 = node1.get_next_node()
+
+  while node2 is not None:
+    if node2.get_value() == val2:
+      break
+    node2_prev = node2
+    node2 = node2.get_next_node()
+
+  if (node1 is None or node2 is None):
+    print("Swap not possible - one or more element is not in the list")
+    return
+
+  if node1_prev is None:
+    input_list.head_node = node2
+  else:
+    node1_prev.set_next_node(node2)
+
+  if node2_prev is None:
+    input_list.head_node = node1
+  else:
+    node2_prev.set_next_node(node1)
+
+  temp = node1.get_next_node()
+  node1.set_next_node(node2.get_next_node())
+  node2.set_next_node(temp)
+
+
+ll = LinkedList.LinkedList()
+for i in range(10):
+  ll.insert_beginning(i)
+
+print(ll.stringify_list())
+swap_nodes(ll, 9, 5)
+print(ll.stringify_list())
+
+
+##################
+# Runner Technique - find the nth last element
+# Finishes with:
+# - O(n) time, as we need to loop through the list at least once;
+# - O(1) space, as we utilise just 3 variables, regardless of the size of the list
+
+def nth_last_node(linked_list, n):
+  nth = None
+  tail_pointer = linked_list.get_head_node()
+  count = 1
+
+  while(tail_pointer != None):
+    tail_pointer = tail_pointer.get_next_node()
+    count += 1
+    if count >= n + 1:
+      if nth == None:
+        nth = linked_list.get_head_node()
+      else:
+        nth = nth.get_next_node()
+
+  return nth
+
+def generate_test_linked_list():
+  linked_list = LinkedList()
+  for i in range(50, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list()
+print(test_list.stringify_list())
+nth_last = nth_last_node(test_list, 4)
+print(nth_last.value)
+
+##################
+# Similar Problem - Find the middle node!
+# Also finishes with:
+# - O(n) time, as we need to loop through the list at least once;
+# - O(1) space, as we just use 2 variables
+# Note: we can also do half speed approach, which is basically equal to 2 x speed approach
+def find_middle(linked_list):
+  p1 = linked_list.get_head_node()
+  p2 = linked_list.get_head_node()
+  while(p1):
+    p1 = p1.get_next_node()
+    if(p1):
+      p1 = p1.get_next_node()
+      p2 = p2.get_next_node()
+  return p2
+
+def generate_test_linked_list(length):
+  linked_list = LinkedList()
+  for i in range(length, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list(9)
+print(test_list.stringify_list())
+middle_node = find_middle(test_list)
+print(middle_node.value)
