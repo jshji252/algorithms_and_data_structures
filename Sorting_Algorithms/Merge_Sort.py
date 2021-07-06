7940a5e57b9d9ebbff8440af49e1293f3dd6205d
jshji252 <jasonseunghoonji@gmail.com>
2021-07-06 17:37:15 +1000

    Merge sort added

---
+++ Sorting_Algorithms/Merge_Sort.py
@@ -0,0 +1,32 @@
+def merge_sort(items):
+  print(items)
+  if len(items) <= 1:
+    return items
+
+  middle_index = len(items) // 2
+  left_split = items[:middle_index]
+  right_split = items[middle_index:]
+
+  left_sorted = merge_sort(left_split)
+  right_sorted = merge_sort(right_split)
+
+  return merge(left_sorted, right_sorted)
+
+def merge(left, right):
+  result = []
+  while (left and right):
+    if left[0] < right[0]:
+      result.append(left[0])
+      left.pop(0)
+    else:
+      result.append(right[0])
+      right.pop(0)
+
+  if left:
+    result += left
+  if right:
+    result += right
+  print(result)
+  return result
+
+print(merge_sort([98,13,274,7,12,981,5]))
--- Sorting_Algorithms/README.md
+++ Sorting_Algorithms/README.md
@@ -10,3 +10,13 @@ The easiest and most straight forward sorting algorithm. When the data is alread
 2. Repeat the following until sorted:
    1. If not in the order yet, swap
    2. If in order, move on
+
+## Merge Sort
+
+Using the _Divide and Conquer_ method, recurse until we reach the base case of a list with single element, then we merge. It's notable characteristic is that it has the same best, worst, average time complexity.
+
+1. Check if the input list contains one element only (**base case**)
+2. Split the list in half; left / right
+3. Recurse the left/right to get left_sorted / right_sorted
+4. Return merge(left_sorted, right_sorted)
+5. Note the helper function merge()
